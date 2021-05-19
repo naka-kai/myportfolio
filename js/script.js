cc8f4a5b12e27bf0c614c4fa37aaccb73688a662
kai <sea.k.naka@gmail.com>
2021-05-19 21:02:59 +0900

    responsive hum pagelink

--- js/script.js
+++ js/script.js
@@ -11,11 +11,22 @@ jQuery(function() {
   });
 
   //ハンバーガーメニュー
-  jQuery('.burger-btn').on('click', function() {
-    jQuery('.burger-btn, .bar-top, .bar-mid, .bar-bottom').toggleClass('active');
-    jQuery('.header_gnav-wrapper').toggleClass('slide-in');
-    jQuery('body').toggleClass('noscroll');
-  });
+    jQuery('.burger-btn').on('click', function() {
+      jQuery('.burger-btn, .bar-top, .bar-mid, .bar-bottom').toggleClass('active');
+      jQuery('.header_gnav-wrapper').toggleClass('slide-in');
+      jQuery('body').toggleClass('noscroll');
+    });
+
+  if (window.matchMedia("(min-width: 1280px)").matches) {
+    jQuery('body').removeClass('noscroll');
+  }
+
+  //ハンバーガーメニューのナビリンククリック後自動で閉じる
+  if (window.matchMedia("(max-width: 1280px)").matches){
+    $('.header_listItem a').on('click', function() {
+      $('.burger-btn').click();
+    });
+  }
 
 
 });
